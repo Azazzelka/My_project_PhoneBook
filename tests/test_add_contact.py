33d49427026a0420088fc1a7241cb80bc54cd1af
Vladimir Zavodyanny <zavodyanny@icloud.com>
2026-08-26 16:36:09 +0300

    phoneBook update

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,11 +1,10 @@
 from time import sleep
 
 import pytest
-
+from selenium.common import TimeoutException
 
 from pages.home_page import HomePage
-from tests_data.contact_test_data import VALID_CONTACT_DATA
-
+from tests_data.contact_test_data import VALID_CONTACT_DATA, INVALID_CONTACT_DATA_WITH_EMPTY_FIELD
 
 
 @pytest.mark.parametrize("contact", VALID_CONTACT_DATA)
@@ -15,3 +14,28 @@ def test_add_contact_success_all_fields(authenticated_driver, contact):
     add_contact_page.fill_add_contact_form(contact)
 
     add_contact_page.wait_until_contact_add_form_closed()
+
+
+@pytest.mark.parametrize("contact, expected_behavior, expected_message", INVALID_CONTACT_DATA_WITH_EMPTY_FIELD)
+def test_add_contact_with_empty_field(authenticated_driver, contact, expected_behavior, expected_message):
+    add_contact_page = HomePage(authenticated_driver).open_add_contact_form()
+    add_contact_page.fill_add_contact_form(contact)
+
+    if expected_behavior == "alert":
+        try:
+            actual_message = add_contact_page.get_alert_text()
+        except TimeoutException:
+            pytest.fail(f"Expected alert did not appear"
+                        f"{expected_message!r}",
+                        pytrace = False)
+
+
+
+        assert expected_message in actual_message, (
+        f"Expected message: {expected_message!r}, "
+        f"actual message: {actual_message!r}")
+
+        add_contact_page.alert_accept()
+
+    elif expected_behavior == "Form_closed":
+        assert  add_contact_page.wait_until_contact_add_form_closed()
--- tests_data/contact_test_data.py
+++ tests_data/contact_test_data.py
@@ -1,3 +1,6 @@
+from dataclasses import replace
+
+import pytest
 from faker import Faker
 
 from models.contact import Contact
@@ -14,3 +17,57 @@ VALID_CONTACT_DATA = [Contact(
 )
     for _ in range(4)
 ]
+
+
+def create_contact(**overrides) -> Contact:
+    data = {
+        "name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "phone": fake.unique.numerify("05########"),
+        "email": fake.unique.email(),
+        "address": fake.address(),
+        "description": fake.sentence(),
+            }
+    data.update(overrides)
+    return Contact(**data)
+
+INVALID_CONTACT_DATA_WITH_EMPTY_FIELD = [
+    pytest.param(
+        replace(create_contact(), name=""),
+        "alert",
+        "Name cannot be empty!",
+        id = "empty-name",
+    ),
+    pytest.param(
+        replace(create_contact(), last_name=""),
+        "alert",
+        "Last Name cannot be empty!",
+        id = "empty-last-name",
+    ),
+    pytest.param(
+        replace(create_contact(), phone=""),
+        "alert",
+        "Phone not valid",
+        id = "empty-phone",
+    ),
+    pytest.param(
+        replace(create_contact(), email=""),
+        "alert",
+        "Email not valid",
+        id= "empty-email",
+    ),
+    pytest.param(
+        replace(create_contact(), address=""),
+        "alert",
+        "Address cannot be empty!",
+        id = "empty-address",
+    ),
+    pytest.param(
+        replace(create_contact(), description=""),
+        "Form_closed",
+        None,
+        id = "empty-description",
+    )
+
+]
+
